c0652a9c93404fe41e02ca380ac8ab7ddce9acd6
Lee Katz <lskatz@gmail.com>
2018-04-22 10:33:36 -0400

    more complete subroutines

--- scripts/taxonomy.pl
+++ scripts/taxonomy.pl
@@ -17,50 +17,90 @@ sub main{
 
   die usage() if(!@ARGV || $$settings{help});
 
-  my $db;
-  if(-e $$settings{dbpath}){
-    $db=readTaxonomyDb($$settings{dbpath},$settings);
-  } else {
-    $db = loadTaxonomy($ARGV[0],$$settings{dbpath},$settings);
-  }
+  my $dbdir=$ARGV[0];
 
-  my ($key,$value)=each(%$db);
-  die Dumper [$key,$value];
 
-}
+  my @nodesKeys=qw(tax_id parent_tax_id rank embl_code division_id inherited_div_flag genetic_code_id inherited_gc_flag mitochondrial_genetic_code_id inherited_mgc_flag genbank_hidden_flag hidden_subtree_root_flag comments);
+  my @namesKeys=qw(tax_id name_txt unique_name name_class);
 
-sub loadTaxonomy{
-  my($db,$dbpath,$settings)=@_;
-  my $nodes="$db/nodes.dmp";
-  my $names="$db/names.dmp";
-  my(%nodes,%names);
-  tie %nodes, "BerkeleyDB::Hash",
-      -Filename => $dbpath,
-      -Flags    => DB_CREATE
-      or die "Cannot open file $dbpath: $! $BerkeleyDB::Error\n" ;
+  # The subroutines strip away the tax_id
+  shift(@nodesKeys);
+  shift(@namesKeys);
 
-  open(my $namesFh, $names) or die "ERROR: could not read $names.dmp: $!";
-  while(my $line=<$namesFh>){
-    $line=~s/^\s*|\s*$//g; # trim
-    $line=~s/^\||\|$//g;   # pipe trim
-    $line=~s/^\s*|\s*$//g; # trim
-    $line=~s/^\||\|$//g;   # pipe trim
-    my @F=split(/\t\|\t/,$line);
-    $names{$F[0]}=\@F;
-  }
-  close $namesFh;
+  my $names = readDmpHashOfArr("$dbdir/names.dmp",$settings);
 
-  open(my $nodesFh, $nodes) or die "ERROR: could not read $nodes.dmp: $!";
-  while(my $line=<$nodesFh>){
-    $line=~s/^\s*|\s*$//g; # trim
-    $line=~s/^\||\|$//g;   # pipe trim
-    $line=~s/^\s*|\s*$//g; # trim
-    $line=~s/^\||\|$//g;   # pipe trim
-    my @F=split(/\t\|\t/,$line);
-    $nodes{$F[0]}=[@F,@{ $names{$F[0]} }];
+  my %nodesDb;
+  tie %nodesDb, "BerkeleyDB::Hash",
+      -Filename => $$settings{dbpath},
+      -Flags    => DB_TRUNCATE
+      or die "Cannot open file $$settings{dbpath}: $! $BerkeleyDB::Error\n" ;
+  
+  open(my $nodesFh,"$dbdir/nodes.dmp") or die "ERROR: could not read nodes.dmp: $!";
+  while(my $nodesArr=taxonomyIterator($nodesFh,$settings)){
+    my $taxid=shift(@$nodesArr);
+    if(!defined($$names{$taxid})){
+      logmsg "Warning: tax_id $taxid not defined in names.dmp";
+      $$names{$taxid}=[];
+    }
+    my $line=join("\t",@$nodesArr,@{$$names{$taxid}});
+    $nodesDb{$taxid}=$line;
   }
   close $nodesFh;
-  return \%nodes;
+  untie %nodesDb; # kind of deletes the hash but flushes db
+  
+  return 0; # exit status 0
+}
+# Read a dmp file into a hash whose values are an array
+# of each element in the dmp, except the first which is
+# used as the hash key.
+sub readDmpHashOfHash{
+  my($dmp,$keys,$settings)=@_;
+  my %tax;
+  my @keys=@$keys; # copy into local space in case it helps speed up
+  open(my $dmpFh,$dmp) or die "ERROR reading $dmp: $!";
+  while(<$dmpFh>){
+    s/(\s*\|\s*)+$//g; # right trim
+    my %F;
+    @F{@keys} = split /\t\|\t/;
+    my $id=$F{tax_id};
+    delete($F{tax_id}); # reduce memory footprint
+
+    # The entry's value will be an array with the ID
+    # lopped off.
+    $tax{$id}=\%F;
+  }
+  close $dmpFh;
+  return \%tax;
+}
+
+# Read a dmp file into a hash whose values are an array
+# of each element in the dmp, except the first which is
+# used as the hash key.
+sub readDmpHashOfArr{
+  my($dmp,$settings)=@_;
+  my %tax;
+  open(my $dmpFh,$dmp) or die "ERROR reading $dmp: $!";
+  while(<$dmpFh>){
+    s/(\s*\|\s*)+$//g; # right trim
+    my @F = split /\t\|\t/;
+
+    # The entry's value will be an array with the ID
+    # lopped off.
+    $tax{$F[0]} = [@F[1..$#F]];
+  }
+  close $dmpFh;
+  return \%tax;
+}
+
+sub taxonomyIterator{
+  my($fh,$settings)=@_;
+  if(eof($fh)){
+    return undef;
+  }
+
+  my $line=<$fh>;
+  $line=~s/(\s*\|\s*)+$//g; # right trim
+  return [split /\t\|\t/,$line];
 }
 
 sub readTaxonomyDb{
